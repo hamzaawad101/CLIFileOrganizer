42a7a22466cab5eb7f95ffc18869161c9028204f
hamzaawad101 <hamzaawad101a@gmail.com>
2025-12-15 15:14:30 -0500

    replaced input with command-line arg

--- file_organizer.py
+++ file_organizer.py
@@ -1,21 +1,40 @@
 import os
 import shutil
+import sys
 
 
-target_folder=input("Enter path for folder: ")
+if len(sys.argv)<2:
+    print("Usage: python file_organizer.py /path/to/folder")
+    exit()
+
+target_folder=sys.argv[1]
 print(target_folder)
+
 
 #check if path exists
 if os.path.exists(target_folder) and os.path.isdir(target_folder):
     extensions={item.split('.')[-1]for item in os.listdir(target_folder)if os.path.isfile(os.path.join(target_folder,item))}
 
 
-    print(extensions)
+    print(f"Found {len(extensions)} extensions: {extensions}")
 
-    #create folders for each extension type
+    created_folders = 0
+
+    # create folders for each extension type
     for extension in extensions:
-        if not os.path.exists(os.path.join(target_folder,extension)):
-            os.makedirs(os.path.join(target_folder,extension))
+        folder_path = os.path.join(target_folder, extension)
+        if not os.path.exists(folder_path):
+            os.makedirs(folder_path)
+            created_folders += 1
+
+    # check for no_extension folder
+    if any('.' not in item for item in os.listdir(target_folder) if os.path.isfile(os.path.join(target_folder,item))):
+        no_ext_path = os.path.join(target_folder, "no_extension")
+        if not os.path.exists(no_ext_path):
+            os.makedirs(no_ext_path)
+            created_folders += 1
+
+    print(f"Created {created_folders} new folders")
 
     #move files to designated folders
     for item in os.listdir(target_folder):
@@ -42,7 +61,7 @@ if os.path.exists(target_folder) and os.path.isdir(target_folder):
             dest_path = os.path.join(target_folder, file_extension, new_name)
             counter += 1
 
-        # finally move the file
+        #  move the file
         shutil.move(item_path, dest_path)
 
 #check if path is directory or file
