89b6fcd7ef38c030af36f15c6464c550f0cdfd3a
hamzaawad101 <hamzaawad101a@gmail.com>
2025-12-15 14:45:23 -0500

    added invalid input handling

--- file_organizer.py
+++ file_organizer.py
@@ -6,7 +6,7 @@ target_folder=input("Enter path for folder: ")
 print(target_folder)
 
 #check if path exists
-if os.path.exists(target_folder) & os.path.isdir(target_folder):
+if os.path.exists(target_folder) and os.path.isdir(target_folder):
     extensions={item.split('.')[-1]for item in os.listdir(target_folder)if os.path.isfile(os.path.join(target_folder,item))}
 
 
@@ -19,9 +19,32 @@ if os.path.exists(target_folder) & os.path.isdir(target_folder):
 
     #move files to designated folders
     for item in os.listdir(target_folder):
-        if os.path.isfile(os.path.join(target_folder,item)):
+        item_path = os.path.join(target_folder, item)
+
+        # explicitly skip directories
+        if os.path.isdir(item_path):
+            continue
+        if '.' in item:
             file_extension=item.split('.')[-1]
-            shutil.move(os.path.join(target_folder,item),os.path.join(target_folder,file_extension,item))
+        else:
+            file_extension="no_extension"
+        folder_path = os.path.join(target_folder, file_extension)
+        if not os.path.exists(folder_path):
+            os.makedirs(folder_path)
+
+        #move file into folder
+        dest_path = os.path.join(target_folder, file_extension, item)
+        counter = 1
+        # check if file with same name exists
+        while os.path.exists(dest_path):
+            base_name, ext = os.path.splitext(item)  # split "file.txt" into "file" and ".txt"
+            new_name = f"{base_name}_{counter}{ext}"  # create "file_1.txt", "file_2.txt", etc.
+            dest_path = os.path.join(target_folder, file_extension, new_name)
+            counter += 1
+
+        # finally move the file
+        shutil.move(item_path, dest_path)
+
 #check if path is directory or file
 elif os.path.isfile(target_folder):
         print("Path entered is a file.Path must be a folder")
